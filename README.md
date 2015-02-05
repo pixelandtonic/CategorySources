16b6c13dbfe8c0bddc78311eef3fa40e9eab2f11
Brandon Kelly <brandon@pixelandtonic.com>
2015-02-05 13:15:17 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Category Sources plugin for Craft
+
+This plugin makes it possible to view your entries by their category-based taxonomies, by adding category-based sources to entries’ source lists.
+
+![An entry source list with two category groups](screenshot.png)
+
+
+## Installation
+
+To install Category Sources, follow these steps:
+
+1.  Upload the categorysources/ folder to your craft/plugins/ folder.
+2.  Go to Settings > Plugins from your Craft control panel and enable the Category Sources plugin.
+3.  Click on “Category Sources” to go to the plugin’s settings page, and configure the plugin how you’d like.
---
+++ categorysources/CategorySourcesPlugin.php
@@ -0,0 +1,114 @@
+<?php
+namespace Craft;
+
+class CategorySourcesPlugin extends BasePlugin
+{
+	function getName()
+	{
+		return Craft::t('Category Sources');
+	}
+
+	function getVersion()
+	{
+		return '1.0';
+	}
+
+	function getDeveloper()
+	{
+		return 'Pixel & Tonic';
+	}
+
+	function getDeveloperUrl()
+	{
+		return 'http://pixelandtonic.com';
+	}
+
+	protected function defineSettings()
+	{
+		return array(
+			'categoryGroups' => array(AttributeType::Mixed, 'default' => array()),
+		);
+	}
+
+	public function getSettingsHtml()
+	{
+		$groups = craft()->categories->getAllGroups();
+		$selectedGroups = $this->getSettings()->categoryGroups;
+		$groupOptions = array();
+
+		foreach ($groups as $group)
+		{
+			$groupOptions[] = array('label' => $group->name, 'value' => $group->id);
+		}
+
+		return craft()->templates->renderMacro('_includes/forms', 'checkboxSelectField', array(
+			array(
+				'first' => true,
+				'label' => 'Category Groups',
+				'instructions' => 'Choose which category groups should be included in entry source lists.',
+				'options' => $groupOptions,
+				'values' => $selectedGroups,
+			)
+		));
+	}
+
+	public function prepSettings($settings)
+	{
+		if ($settings['categoryGroups'] == '*')
+		{
+			unset($settings['categoryGroups']);
+		}
+
+		return $settings;
+	}
+
+	public function modifyEntrySources(&$sources, $context)
+	{
+		$selectedGroups = $this->getSettings()->categoryGroups;
+
+		if (!$selectedGroups)
+		{
+			$selectedGroups = craft()->categories->getAllGroupIds();
+		}
+
+		foreach ($selectedGroups as $groupId)
+		{
+			$group = craft()->categories->getGroupById($groupId);
+			$sources[] = array('heading' => Craft::t($group->name));
+
+			$criteria = craft()->elements->getCriteria(ElementType::Category);
+			$criteria->groupId = $groupId;
+			$categories = craft()->elements->findElements($criteria);
+			$lastSourceByLevel = array();
+
+			foreach ($categories as $category)
+			{
+				$l = 'l'.$category->level;
+
+				if ($l == 'l1')
+				{
+					$levelSources = &$sources;
+				}
+				else
+				{
+					$parentL = 'l'.($category->level-1);
+
+					if (!isset($lastSourceByLevel[$parentL]['nested']))
+					{
+						$lastSourceByLevel[$parentL]['nested'] = array();
+					}
+
+					$levelSources = &$lastSourceByLevel[$parentL]['nested'];
+				}
+
+				$levelSources['cat:'.$category->id] = array(
+					'label' => $category->title,
+					'criteria' => array('relatedTo' => array('targetElement' => $category->id))
+				);
+
+
+				$lastSourceByLevel[$l] = &$levelSources['cat:'.$category->id];
+			}
+		}
+	}
+}
